ab408ec5fcf5bb5e916c4966cc05c066e1dc853d
asif2703/ git config --global user.name asif2703 <wapasif@gmail.com>
2020-08-06 17:32:43 +0600

    middle range

---
+++ letConst.js
@@ -0,0 +1,20 @@
+const wifeName = "Lipi";
+console.log(wifeName);
+
+const numbers = [12,15,85];
+numbers.pop(15);
+numbers.push(99);
+numbers[2] = 100;
+console.log(numbers);
+const nayok = {Name:"sakib khan", phone:569852};
+console.log(nayok);
+
+let patientName = 'A.Rahim chacha';
+patientName = "Rahima khala";
+console.log(patientName);
+
+let sum = 9;
+for (let i = 0; i < 10; i++) {
+    sum = sum +i;
+    console.log(sum);
+}
---
+++ templete.js
@@ -0,0 +1,10 @@
+const firstName = "Asif";
+const lastName = "Iqbal";
+const fullName = (firstName +" "+ lastName +'is a good boy');
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+console.log(fullName2);
+
+const multiLine = `how are u?
+what's your name?
+where are you ?`;
+console.log(multiLine);
